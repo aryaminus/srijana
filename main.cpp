8d01ad769ce7acfe60cd24a6106af4259c131eed
Sunim Acharya <sunim.54@gmail.com>
2017-01-27 02:22:44 -0800

    Start text  for console

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+
+int main(){
+    cout << "-----------------------------------------------" << endl;
+	cout << endl;
+	cout << "       --- Srijana - A OpenGL & NN Game ---    " << endl;
+	cout << endl;
+	cout << "written by: Sunim Acharya(72080)in cpp in linux " << endl;
+	cout << endl;
+	cout << "-----------------------------------------------" << endl;
+
+}
