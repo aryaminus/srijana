29999c4a7d55825c62cc2404b845c503c69a97aa
Sunim Acharya <sunim.54@gmail.com>
2017-02-08 09:10:38 +0545

    Setup main class to fetch variables

--- neural.cpp
+++ neural.cpp
@@ -15,3 +15,19 @@ typedef struct layer{
 	int num_nodes;
 	node *chr;
 } layer;
+
+using namespace std;
+
+class neural{
+	private:
+		int num_inputs;
+		int num_outputs;
+		int num_layers;
+		int num_weights;
+		int num_hid_nodes;
+		float leaning_rate;
+		layer *layers;
+		float *weights;
+		
+	public:
+};
