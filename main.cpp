0c240279c292b38dc88b332d41bc904fd6c112ba
Sunim Acharya <sunim.54@gmail.com>
2017-02-27 03:10:38 +0545

    Setup add function to  border values

--- main.cpp
+++ main.cpp
@@ -21,6 +21,8 @@ typedef struct sq{
 
 sq *snake = NULL;
 
+int mx;
+int my;
 neural *net;
 
 int num_layers   =      2;
@@ -31,7 +33,23 @@ float learning_rate     = 0.0000001;
 
 void start(){
 	snake = NULL;
+    add(0, 0);
+	add(1, 0);
+	add(2, 0);
+	add(3, 0);
+	add(4, 0);
+	mx = 1;
+	my = 0;
+}
 
+void add(int x, int y){
+	sq *tmp = (sq *)malloc(sizeof(sq)); // Start sq snake tmp with Memory Allocation
+	tmp -> x = x;
+	tmp -> y = y;
+	tmp -> mx = 1;
+	tmp -> my = 0;
+	tmp -> nexploration_ratet = snake;
+	snake = tmp;
 }
 
 int main(){
