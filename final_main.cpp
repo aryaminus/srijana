a8a372d3ee999ec1b3747faefc60bd469b3ce1bc
Sunim Acharya <sunim.54@gmail.com>
2018-01-27 19:48:48 +0545

    Using reshape and timer properties

--- final_main.cpp
+++ final_main.cpp
@@ -8,20 +8,50 @@
  
 /* Handler for window-repaint event. Call back when the window first appears and
    whenever the window needs to be re-painted. */
+
+int refreshMills = 30;
+
 void display() {
-   //glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
    glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer (background)
 
    glFlush();  // Render now
 }
  
+void myReshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integer
+   // Compute aspect ratio of the new window
+   if (height == 0) height = 1;                // To prevent divide by 0
+   GLfloat aspect = (GLfloat)width / (GLfloat)height;
+ 
+   // Set the viewport to cover the new window
+   glViewport(0, 0, width, height);
+ 
+   // Set the aspect ratio of the clipping area to match the viewport
+   glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
+   glLoadIdentity();
+   if (width >= height) {
+     // aspect >= 1, set the height from -1 to 1, with larger width
+      gluOrtho2D(-1.0 * aspect, 1.0 * aspect, -1.0, 1.0);
+   } else {
+      // aspect < 1, set the width to -1 to 1, with larger height
+     gluOrtho2D(-1.0, 1.0, -1.0 / aspect, 1.0 / aspect);
+   }
+}
+
+void Timer(int value) {
+   glutPostRedisplay();      // Post re-paint request to activate display()
+   glutTimerFunc(refreshMills, Timer, 0); // next Timer call milliseconds later
+}
+
 /* Main function: GLUT runs as a console application starting at main()  */
 int main(int argc, char** argv) {
    glutInit(&argc, argv);                 // Initialize GLUT
+   glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH); // Use RGBA color, enable double buffering and enable depth buffer
    glutCreateWindow("Srijana: User and Neural Network game"); // Create a window with the given title
    glutInitWindowSize(800, 600);   // Set the window's initial width & height
    glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
    glutDisplayFunc(display); // Register display callback handler for window re-paint
+   glutReshapeFunc(myReshape);       // Register callback handler for window re-size event
+   glutTimerFunc(0, Timer, 0);     // First timer call immediately
    glutMainLoop();           // Enter the event-processing loop
    return 0;
 }
