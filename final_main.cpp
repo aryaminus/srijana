4bc22b1967f4cbcfb3cb45efc70eeaf9ef1805cf
Sunim Acharya <sunim.54@gmail.com>
2018-01-27 19:36:00 +0545

    Window create

---
+++ final_main.cpp
@@ -0,0 +1,27 @@
+/*
+ * final_main.cpp: OpenGL/GLUT C/C++ Setup graphics project
+ * To compile with -lfreeglut -lglu32 -lopengl32
+ */
+
+//#include <windows.h>  // for MS Windows
+#include <GL/glut.h>  // GLUT, include glu.h and gl.h
+ 
+/* Handler for window-repaint event. Call back when the window first appears and
+   whenever the window needs to be re-painted. */
+void display() {
+   //glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
+   glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer (background)
+
+   glFlush();  // Render now
+}
+ 
+/* Main function: GLUT runs as a console application starting at main()  */
+int main(int argc, char** argv) {
+   glutInit(&argc, argv);                 // Initialize GLUT
+   glutCreateWindow("Srijana: User and Neural Network game"); // Create a window with the given title
+   glutInitWindowSize(800, 600);   // Set the window's initial width & height
+   glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
+   glutDisplayFunc(display); // Register display callback handler for window re-paint
+   glutMainLoop();           // Enter the event-processing loop
+   return 0;
+}
