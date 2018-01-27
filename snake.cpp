5b1e14bdff0d1c81f2b3d5b4f18983797e5577b8
Sunim Acharya <sunim.54@gmail.com>
2018-01-27 14:12:25 +0545

    Add Airplane layout

--- snake.cpp
+++ snake.cpp
@@ -83,9 +83,75 @@ void draw_string(void *font, const char* string)
         glutStrokeCharacter(font, *string++);
 }
 
+void drawString(float x,float y,float z,void *font,char *string)
+{
+	char *c;
+	glRasterPos3f(x, y,z);
+
+	for (c=string; *c != '\0'; c++)
+	{
+		glutBitmapCharacter(font, *c);
+	}
+}
+
 void DrawMenu()
 {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glColor3f(0.3,0.56,0.84);   //welcome background
+	glBegin(GL_POLYGON);
+	glVertex3f(0.0,0.0,0.0);
+	glColor3f(0.137,0.137,0.556);
+	glVertex3f(100.0,0.0,0.0);
+	glColor3f(0.196,0.196,0.8);
+	glVertex3f(100.0,100.0,0.0);
+	glVertex3f(0.0,100.0,0.0);
+	glEnd();
+
+	// button 1 .. PLAY
+	glColor3f(0.196,0.196,0.8);
+	glRectf(39.5,39.5,60.5,45.5);
+
+	glColor3f(0.8,0.8,0.8);
+	glRectf(40,40,60,45);
+	glColor3f(0.137,0.137,0.556);
+	drawString(47,42,0,GLUT_BITMAP_HELVETICA_18,"PLAY");
+
+	// button 2 .. instructions
+	glColor3f(0.196,0.196,0.8);
+	glRectf(39.5,29.5,60.5,35.5);
+
+	glColor3f(0.8,0.8,0.8);
+	glRectf(40,30,60,35);
+	glColor3f(0.137,0.137,0.556);
+	drawString(41,31,0,GLUT_BITMAP_HELVETICA_18,"INSTRUCTIONS");
+
+	// button 3 .. ABOUT
+	glColor3f(0.196,0.196,0.8);
+	glRectf(39.5,19.5,60.5,25.5);
+
+	glColor3f(0.8,0.8,0.8);
+	glRectf(40,20,60,25);
+	glColor3f(0.137,0.137,0.556);
+	drawString(46,21,0,GLUT_BITMAP_HELVETICA_18,"ABOUT");
+
+	// button 4 .. exit
+	glColor3f(0.196,0.196,0.8);
+	glRectf(39.5,9.5,60.5,15.5);
+
+	glColor3f(0.8,0.8,0.8);
+	glRectf(40,10,60,15);
+	glColor3f(0.137,0.137,0.556);
+	drawString(47,11,0,GLUT_BITMAP_HELVETICA_18,"EXIT");
+
+
+	glPushMatrix();
+
+	glColor3f(0.8,0.8,0.8);
+	drawString(25.5,92,0,GLUT_BITMAP_TIMES_ROMAN_24,"COMPUTER GRAPHICS PROJECT ");
+	drawString(35.5,80,0,GLUT_BITMAP_TIMES_ROMAN_24,"AIRPLANE GAME ");
+	glPopMatrix();
+	glColor3f(0.137,0.137,0.556);
 
     glBegin(GL_POLYGON); // User
     glColor3f (0.0, 0.26, 0.0);
