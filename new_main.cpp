43acfb7eb1b090f7c5698533b646e1e1d2f00e36
Sunim Acharya <sunim.54@gmail.com>
2018-01-27 14:49:18 +0545

    Setup fot init

--- new_main.cpp
+++ new_main.cpp
@@ -7,18 +7,30 @@
 
 int SCREENH=600,SCREENW=800;
 
+void init()
+{
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_COLOR_MATERIAL);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_NORMALIZE);
+	glShadeModel(GL_SMOOTH);
+	glLoadIdentity ();
+	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+
+	GLfloat acolor[] = {1.4, 1.4, 1.4, 1.0};
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, acolor);
+}
+
 int main (int argc,char **argv)
 {
     glutInit (&argc, argv);
     glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize (SCREENW,SCREENH);
-
-    
-    CreateGlutWindow();
-    glutDisplayFunc (display);
-    glutTimerFunc (80,timer,0);
-    glutSpecialFunc (MyKeyboard);
-    glutMouseFunc(MousePressed);
+    glutInitWindowPosition (50,50);
+    glutCreateWindow ("Srijana");
+    init();
 
     return 0;
 }
