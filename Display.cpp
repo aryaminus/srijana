77030a58fb4ce6d5afdaba7def4b279ce665a8db
Sunim Acharya <sunim.54@gmail.com>
2018-01-27 13:20:48 +0545

    Create CodeBlocks class

---
+++ Display.cpp
@@ -0,0 +1,23 @@
+#include "display.h"
+
+Display::Display()
+{
+    //ctor
+}
+
+Display::~Display()
+{
+    //dtor
+}
+
+Display::Display(const Display& other)
+{
+    //copy ctor
+}
+
+Display& Display::operator=(const Display& rhs)
+{
+    if (this == &rhs) return *this; // handle self assignment
+    //assignment operator
+    return *this;
+}
---
+++ display.h
@@ -0,0 +1,18 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+
+class Display
+{
+    public:
+        Display();
+        virtual ~Display();
+        Display(const Display& other);
+        Display& operator=(const Display& other);
+
+    protected:
+
+    private:
+};
+
+#endif // DISPLAY_H
