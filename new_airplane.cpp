65ea2f15bf0ff995f198141f402a0945c29da530
Sunim Acharya <sunim.54@gmail.com>
2018-01-27 15:56:04 +0545

    Window Setup

--- new_airplane.cpp
+++ new_airplane.cpp
@@ -5,3 +5,21 @@
 
 #include <GL/gl.h>
 #include <GL/glut.h>
+
+#define BLOCKSPEED 0.09
+
+#define BOOSTER_MAX 50
+
+int SCREENH=600,SCREENW=800;
+
+int main(int argc, char** argv)
+{
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize (SCREENW,SCREENH);
+	glutInitWindowPosition (50,50);
+	glutCreateWindow ("Airplane Escape!");
+	return 0;
+
+}
