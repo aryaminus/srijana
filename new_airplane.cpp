4caa9f74cdbf9da5e8f02159bdcc16657cdeec59
Sunim Acharya <sunim.54@gmail.com>
2018-01-27 15:59:34 +0545

    Building Block and cloud setup

--- new_airplane.cpp
+++ new_airplane.cpp
@@ -11,6 +11,54 @@
 #define BOOSTER_MAX 50
 
 int SCREENH=600,SCREENW=800;
+int buildColor; 
+
+typedef struct building
+{
+	float block_x,block_y;
+
+	bool state;
+	int no_floors;
+}building;
+
+building b;
+
+typedef struct Cloud
+{
+	float block_x,block_y;
+	bool state;
+}Cloud;
+
+Cloud s;
+
+void buildingBlock()
+{
+	b.block_x=50.0;
+	srand(time(0));
+	b.no_floors = rand()%3+4;
+	buildColor = rand()%3;
+
+	b.block_y=b.no_floors*10 +15;   // generate block y cordinate depending on no of floors
+	b.state=true;
+	s.state=false;
+
+}
+
+void init()
+{
+	srand(time(0));
+	int random = rand()%2;
+	if( random == 0)
+	{
+		buildingBlock();
+	}
+	else
+	{
+		CloudBlock();
+	}
+
+
+}
 
 int main(int argc, char** argv)
 {
@@ -20,6 +68,7 @@ int main(int argc, char** argv)
 	glutInitWindowSize (SCREENW,SCREENH);
 	glutInitWindowPosition (50,50);
 	glutCreateWindow ("Airplane Escape!");
+    init();
 	return 0;
 
 }
